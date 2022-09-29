5cd4c64a23e58515af90818a4f03db90fc119e52
joaoCeilandia <joaovitordmrosa@hotmail.com>
2022-09-29 01:28:39 -0300

    add redux to persist login

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,27 @@
 import type { NextPage } from 'next'
 import Image from 'next/image'
+import { useSelector, useDispatch } from 'react-redux'
 import { useState } from 'react'
 import { Input } from '../src/components/Input'
+import { userUpdate } from '../store/actions/users'
 import styles from '../styles/Login.module.css'
 import axios from 'axios'
 
 const Home: NextPage = () => {
-
+  const dispatch = useDispatch();
+  const {id, token, email} = useSelector((state: any) => state.user);
+  console.log({id, token, email})
   const [user, setUser] = useState('')
   const [password, setPassword] = useState('')
 
   const login = () => {
-    axios.post('localhost:3001/auth/login', {
-      email: 'Monsen',
-      password: 'teste'
+    axios.post('//localhost:3001/auth/login', {
+      email: user,
+      password
     })
     .then(function (response) {
       console.log(response);
+      dispatch(userUpdate(response))
     })
     .catch(function (error) {
       console.error(error);
@@ -28,8 +33,7 @@ const Home: NextPage = () => {
         <Image
           src="/homeimg.svg"
           alt="Home img"
-          width="626"
-          height="330"
+          layout="fill"
         />
       <div className={styles.inputSide}>
         <div className={styles.logoText}>
@@ -40,7 +44,7 @@ const Home: NextPage = () => {
         </div>
         <Input title="Usuário" type="email" onChange={e => setUser(e.target.value)}/>
         <Input title="Senha" type="password" onChange={e => setPassword(e.target.value)}/>
-        <button onClick={login}>Entrar</button>
+        <button className={styles.submitButton} onClick={login}>Entrar</button>
       </div>
     </div>
   )
