2e87d52c8dae02527f038a3d32bd50fd81319c99
joaoCeilandia <joaovitordmrosa@hotmail.com>
2023-01-13 06:08:09 -0300

    fix header paths

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -2,6 +2,7 @@ import { SmileFilled } from "@ant-design/icons";
 import { Dropdown, Space } from "antd";
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import React from "react";
 import { useSelector } from "react-redux";
 import { appRoutes } from "../../../constants";
@@ -13,11 +14,12 @@ export function Header() {
     enums: { userType },
   } = useSelector((state: any) => state);
 
+  const router = useRouter();
+
   const headerOptions = {
     [userType?.teacher?.value]: [
-      { label: "Cadastrar turma", href: appRoutes.registerClass },
-      { label: "Indicadores", href: appRoutes.registerClass },
-      { label: "Formulários", href: appRoutes.registerClass },
+      { label: "Indicadores", href: appRoutes.allIndicators },
+      { label: "Formulários", href: appRoutes.home },
       { label: "Turmas", href: appRoutes.classes },
     ],
     [userType?.admin?.value]: [
@@ -35,7 +37,7 @@ export function Header() {
   const onClick = (e: any) => {
     if (e.key) window.location.href = e.key;
   };
-  
+
   return (
     <div className={styles.header}>
       <Image
@@ -44,11 +46,12 @@ export function Header() {
         alt="UnB + A3M imagem"
         width="128"
         height="76"
+        onClick={() => (router.push(appRoutes.home))}
       />
       <Space size={16} style={{ display: "flex", alignItems: "center" }}>
         {headerOptions[user?.userType]?.map(({ label, href }, index) => (
-          <Link key={index} className={styles.link} href={`${href}`}>
-            <a>{label}</a>
+          <Link key={index} className={styles.link} href={href}>
+            {label}
           </Link>
         ))}
         <Dropdown menu={{ items, onClick }} trigger={["click"]}>
